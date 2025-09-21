11b07e3d33cebdc79a3290bf6aaf2754cf233b28
chenyaolin0308@gmail.com <>
2025-09-21 16:37:24 +0800

    修復服務器端地點排序，只對實際存在的地點進行排序

--- server/src/routes/locations.ts
+++ server/src/routes/locations.ts
@@ -7,28 +7,12 @@ const router = Router();
 router.get('/', async (_req, res) => {
   const allLocations = await Location.find();
   
-  // 按照固定順序排列：進貨、上架、庫存調、觀塘、灣仔、荔枝角、元朗、國內倉、總庫
-  const order = ['進貨', '上架', '庫存調', '觀塘', '灣仔', '荔枝角', '元朗', '國內倉', '總庫'];
-  
-  // 創建一個映射來處理可能的名稱變體
-  const nameMapping: { [key: string]: string } = {
-    '國內倉': '國內倉',
-    '國內倉庫': '國內倉',
-    '國內': '國內倉',
-    '元朗': '元朗',
-    '元朗倉': '元朗',
-    '總庫': '總庫',
-    '總倉庫': '總庫',
-    '總計': '總庫'
-  };
+  // 按照固定順序排列：觀塘、灣仔、荔枝角、元朗、國內倉
+  const order = ['觀塘', '灣仔', '荔枝角', '元朗', '國內倉'];
   
   const locations = allLocations.sort((a, b) => {
-    // 使用映射來標準化名稱
-    const aName = nameMapping[a.name] || a.name;
-    const bName = nameMapping[b.name] || b.name;
-    
-    const aIndex = order.indexOf(aName);
-    const bIndex = order.indexOf(bName);
+    const aIndex = order.indexOf(a.name);
+    const bIndex = order.indexOf(b.name);
     
     // 如果找不到匹配的地點名稱，放到最後
     const aOrder = aIndex === -1 ? 999 : aIndex;
